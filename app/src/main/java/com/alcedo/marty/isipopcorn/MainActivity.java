d992c3746eeba402181b400548c23c5f16bb8de0
Martin CORNU <martin.cornu43@gmail.com>
2019-01-30 22:40:01 +0100

    recyclerView with hard coded dataSet

--- app/src/main/java/com/alcedo/marty/isipopcorn/MainActivity.java
+++ app/src/main/java/com/alcedo/marty/isipopcorn/MainActivity.java
@@ -1,14 +1,57 @@
 package com.alcedo.marty.isipopcorn;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.alcedo.marty.isipopcorn.adapter.MovieAdapter;
 
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
+    protected String[] myDataset = new String[4];
+    protected String[] myRealisatorDataset = new String[4];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.main_recycler_view);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        mRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        // specify an adapter (see also next example)
+        myDataset[0] = "bonjour";
+        myDataset[1] = "salut";
+        myDataset[2] = "on est pas là pour êre ici";
+        myDataset[3] = "Je suis un tres tres grand artiste";
+        myRealisatorDataset[0] = "Besson";
+        myRealisatorDataset[1] = "Spielberg";
+        myRealisatorDataset[2] = "Toine";
+        myRealisatorDataset[3] = "Manitaz";
+        mAdapter = new MovieAdapter(myDataset, myRealisatorDataset);
+        mRecyclerView.setAdapter(mAdapter);
+
+    }
+
+    public void launchDetailsActivity(View view) {
+        Intent intent = new Intent(this, DetailsActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/alcedo/marty/isipopcorn/adapter/MovieAdapter.java
@@ -0,0 +1,68 @@
+package com.alcedo.marty.isipopcorn.adapter;
+
+import android.content.res.Resources;
+import android.graphics.Movie;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.alcedo.marty.isipopcorn.R;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MovieViewHolder> {
+    private String[] mDataset;
+    private String[] mRealisatorDataset;
+
+    // Provide a reference to the views for each data item
+    // Complex data items may need more than one view per item, and
+    // you provide access to all the views for a data item in a view holder
+    public static class MovieViewHolder extends RecyclerView.ViewHolder {
+        // each data item is just a string in this case
+        public TextView mTitleTextView;
+        public TextView mRealisatorTextView;
+        public ImageView mMovieImageView;
+
+        public MovieViewHolder(View itemView) {
+            super(itemView);
+            mTitleTextView = itemView.findViewById(R.id.movie_title);
+            mRealisatorTextView = itemView.findViewById(R.id.movie_realisator);
+            mMovieImageView = itemView.findViewById(R.id.movie_image);
+        }
+    }
+
+    // Provide a suitable constructor (depends on the kind of dataset)
+    public MovieAdapter(String[] myDataset, String[] myRealisatorDataset) {
+        mDataset = myDataset;
+        mRealisatorDataset = myRealisatorDataset;
+    }
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public MovieAdapter.MovieViewHolder onCreateViewHolder(ViewGroup parent,
+                                                     int viewType) {
+        // create a new view
+        View v = (View) LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.movie_row_item, parent, false);
+        MovieViewHolder vh = new MovieViewHolder(v);
+        return vh;
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(MovieViewHolder holder, int position) {
+        // - get element from your dataset at this position
+        // - replace the contents of the view with that element
+        holder.mTitleTextView.setText(mDataset[position]);
+        holder.mRealisatorTextView.setText(mRealisatorDataset[position]);
+
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return mDataset.length;
+    }
+}
