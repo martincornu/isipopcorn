a6873e8cf1e54d17c893bd259763749bd8691b21
Martin CORNU <martin.cornu43@gmail.com>
2019-01-30 10:27:51 +0100

    create details empty fagment

---
+++ app/src/main/java/com/alcedo/marty/isipopcorn/DetailsActivity.java
@@ -0,0 +1,14 @@
+package com.alcedo.marty.isipopcorn;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+    }
+}
